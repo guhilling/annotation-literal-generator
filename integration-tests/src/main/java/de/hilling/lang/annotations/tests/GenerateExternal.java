07f3a5d09db0ed15ef1a7e5eb1049ac720e75357
Gunnar Hilling <gunnar@hilling.de>
2018-09-08 12:42:29 +0200

    suppress warnings

--- integration-tests/src/main/java/de/hilling/lang/annotations/tests/GenerateExternal.java
+++ integration-tests/src/main/java/de/hilling/lang/annotations/tests/GenerateExternal.java
@@ -3,5 +3,6 @@ package de.hilling.lang.annotations.tests;
 import de.hilling.lang.annotations.GenerateLiteralFor;
 
 @GenerateLiteralFor(Deprecated.class)
+@SuppressWarnings("squid:S1610")
 abstract class GenerateExternal {
 }
